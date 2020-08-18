f527cd5e8eb128349dfa2edaa3429262947f85d9
cTraymany <chindalatht@gmail.com>
2020-08-18 18:36:49 +0700

    switching to learn IDE

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(params)
+        @name = params[:ships][][:name]
+        @type = params[:ships][][:type]
+        @booty = params[:ships][][:booty]
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        self.all.clear
+    end
 end
